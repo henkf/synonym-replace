38db0981c0893ac145d3f233a3b9ee24ace41808
Henk Freimuth <henk.freimuth@ruhr-uni-bochum.de>
2017-01-23 10:07:07 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Synonym Replace package for Atom
+
+A small plugin that looks up synonyms for a selected word and inserts the selection in place.
+
+![](./scrot.png)
---
+++ spec/synonym-replace-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SynonymReplaceView from '../lib/synonym-replace-view';
+
+describe('SynonymReplaceView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
